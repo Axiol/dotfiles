dad62b9272f7e88c8786b887e9a3c6bcc943e393
Arnaud Delante <contact@arnaud.ninja>
2024-10-04 22:02:44 +0200

    Add CodeCompoanion to nvim

---
+++ nvim/lua/config/codecompanion.lua
@@ -0,0 +1,37 @@
+return {
+	"olimorris/codecompanion.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-treesitter/nvim-treesitter",
+		"hrsh7th/nvim-cmp",                    -- Optional: For using slash commands and variables in the chat buffer
+		"nvim-telescope/telescope.nvim",       -- Optional: For using slash commands
+		{ "stevearc/dressing.nvim", opts = {} }, -- Optional: Improves `vim.ui.select`
+	},
+	config = function()
+		require("codecompanion").setup({
+			strategies = {
+				chat = {
+					adapter = "anthropic",
+				},
+				inline = {
+					adapter = "anthropic",
+				},
+				agent = {
+					adapter = "anthropic",
+				},
+			},
+			adapters = {
+				anthropic = function()
+					return require("codecompanion.adapters").extend("anthropic", {
+						env = {
+							api_key = "cmd: echo $ANTHROPIC_API_KEY",
+						},
+					})
+				end,
+			},
+		})
+
+		vim.api.nvim_set_keymap("n", "<C-a>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
+		vim.api.nvim_set_keymap("v", "<C-a>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
+	end,
+}
