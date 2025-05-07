3e42ff16cddc8914500f1c6b18f448950c27e8d9
Arnaud Delante <contact@arnaud.ninja>
2025-05-07 16:32:59 +0200

    Add dependencies to the install

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 printf "Installing dependencies...\n"
-sudo apt-get install -y ripgrep
+sudo apt-get install -y ripgrep less
 
 printf "Copying files...\n"
 ln -s .config/bash/.bashrc .bashrc
+ln -s .config/zsh/.zshrc .zshrc
